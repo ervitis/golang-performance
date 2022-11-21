0813088e8c7c9d65d8fc5cd0bd261706a3394733
ervitis <victor.martinalonso@hrbrain.co.jp>
2022-11-21 17:54:11 +0900

    refactor

--- cmd/normal_process/main.go
+++ cmd/normal_process/main.go
@@ -6,7 +6,9 @@ import (
 	"github.com/ervitis/golang-performance/common"
 	"github.com/ervitis/golang-performance/infra/metrics"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -23,16 +25,53 @@ func main() {
 	}
 	go func() {
 		<-common.SignalHandler
-		if err := server.Shutdown(context.Background()); err != nil {
+		log.Println("Shutting down server")
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
 			log.Println(err)
 		}
 		close(done)
 	}()
 
-	log.Println("Starting metrics server")
-	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+	go func() {
+		log.Println("Starting metrics server")
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-common.SignalHandler:
+				log.Println("Killing...")
+			default:
+				run()
+				const min, max = 1, 8
+				rndWait := rand.New(rand.NewSource(time.Now().Unix())).Intn(max-min) + min
+				time.Sleep(time.Duration(rndWait) * time.Second)
+			}
+		}
+	}()
+
+	<-done
+	log.Println("End process")
+}
+
+func run() {
+	fo := common.NewFileOperator("/tmp/goperformance/myfulldata.csv")
+	if err := fo.OpenFiles("data"); err != nil {
 		log.Fatal(err)
 	}
-	<-done
-	log.Println("End metric handler")
+
+	users, err := fo.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := fo.Write(users); err != nil {
+		log.Fatal(err)
+	}
 }
